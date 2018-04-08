b9e4ff58c53e8ad691418033240dce70ef948280
FloRul <rumiel.florian@gmail.com>
2018-04-07 21:34:35 -0400

    Set up realtion between BP and C++ controller for AI and Player

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+		auto ControlledTank = GetControlledTank();
+		auto PlayerTank = GetPlayerTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("TankAIController not possessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI Possessing : %s"), *ControlledTank->GetName())
+	}
+
+	if (PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Tank Got : %s"), *PlayerTank->GetName())
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player controller uninstanciated in the current world"))
+
+	}
+
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerController = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	return (PlayerController) ? Cast<ATank>(PlayerController) : nullptr;
+}
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("TankPlayerController not possessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player Possessing : %s"), *ControlledTank->GetName())
+	}
+	
+}
